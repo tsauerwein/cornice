9098ad44ed30b0adf47026ed6cd1d68f55304ce6
Rob Miller <rob@kalistra.com>
2012-03-14 18:24:52 -0700

    don't barf on the isinstance check if the provided decorator is a function and not a class

--- cornice/service.py
+++ cornice/service.py
@@ -246,7 +246,11 @@ class Service(object):
                 # than once for the same function, we need to make sure that
                 # the original function isn't wrapped more than once by the
                 # same functions.
-                if (func is decorator) or (isinstance(func, decorator)):
+                try:
+                    func_is_instance = isinstance(func, decorator)
+                except TypeError:
+                    func_is_instance = False
+                if func is decorator or func_is_instance:
                     # `func` is already one of the provided decorators, assume
                     # we've already wrapped this one and skip it this time
                     break
